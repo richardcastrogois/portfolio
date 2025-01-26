33fb2c1b8b52dc5172786f4921b3a6e95f04be64
richardcastrogois <richard.castrogois@gmail.com>
2025-01-26 20:18:34 -0300

    Etapa 07 concluida

---
+++ app/components/back-to-top/index.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import { TbArrowNarrowUp } from "react-icons/tb"
+import { Button } from "../button"
+import { useCallback, useEffect, useState } from "react";
+
+export const BackToTop = () => {
+  const [show, setShow] = useState(false);
+
+  const scrollToTop = () => {
+    const start = window.scrollY;
+    const duration = 500;
+
+    const startTime = performance.now();
+
+    const scrollSmoothly = (time: number) => {
+      const timeElapsed = time - startTime;
+      const progress = Math.min(timeElapsed / duration, 1);
+      const currentScroll = start - start * progress;
+
+      window.scrollTo(0, currentScroll);
+
+      if (progress < 1) {
+        requestAnimationFrame(scrollSmoothly);
+      }
+    };
+
+    requestAnimationFrame(scrollSmoothly);
+  };
+
+  const handleScroll = useCallback(() => {
+    const scrollPosition = window.scrollY;
+
+    if (!show && scrollPosition > 500) {
+      setShow(true);
+    } else if (show && scrollPosition <= 500) {
+      setShow(false);
+    }
+  }, [show]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [handleScroll]);
+
+  if (!show) return null;
+
+  return (
+    <div className="fixed right-4 bottom-4 z-20">
+      <Button onClick={scrollToTop} className="shadow-lg shadow-emerald-400/20">
+        <TbArrowNarrowUp size={20} />
+      </Button>
+    </div>
+  );
+};
--- app/components/pages/home/hero-section/index.tsx
+++ app/components/pages/home/hero-section/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Button } from "@/app/components/button";
 import { TechBadge } from "@/app/components/tech-badge";
 import Image from "next/image";
@@ -24,6 +26,37 @@ const MOCK_CONTACTS = [
 ]
 
 export const HeroSection = () => {
+    const handleContact = () => {
+        const contactSection = document.querySelector('#contact') as HTMLElement | null;
+        
+        if (contactSection) {
+          const targetPosition = contactSection.offsetTop;
+          
+          const scrollSmoothly = (start: number, end: number) => {
+            const startTime = performance.now();
+            const duration = 500;
+      
+            const animateScroll = (time: number) => {
+              const timeElapsed = time - startTime;
+              const progress = Math.min(timeElapsed / duration, 1);
+              const currentScroll = start + (end - start) * progress;
+              
+              window.scrollTo(0, currentScroll);
+      
+              if (progress < 1) {
+                requestAnimationFrame(animateScroll);
+              }
+            };
+      
+            requestAnimationFrame(animateScroll);
+          };
+      
+          scrollSmoothly(window.scrollY, targetPosition);
+        }
+      };
+      
+      
+
     return (
         <section className="w-full lg:h-[755px] bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col justify-end pb-10 sm:pb-32 py-32 lg:pb-[110px]">
             <div className="container flex items-start justify-between flex-col-reverse lg:flex-row">
@@ -40,7 +73,7 @@ export const HeroSection = () => {
                     </div>
 
                     <div className="mt-6 lg:mt-10 flex sm:items-center sm:gap-5 flex-col sm:flex-row">
-                        <Button className="w-max shadow-button">
+                        <Button className="w-max shadow-button" onClick={handleContact}>
                             Entre em contato
                             <HiArrowNarrowRight size={18} />
                         </Button>
