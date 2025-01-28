4049c7883c1d4690cfcbc7680cb02c05aeec8eec
richardcastrogois <richard.castrogois@gmail.com>
2025-01-28 10:43:07 -0300

    Etapa 11 total concluida

--- app/components/pages/home/work-experience/experience-item.tsx
+++ app/components/pages/home/work-experience/experience-item.tsx
@@ -1,6 +1,8 @@
 import { RichText } from "@/app/components/rich-text"
 import { TechBadge } from "@/app/components/tech-badge"
 import { WorkExperience } from "@/app/types/work-experience"
+import { differenceInMonths, differenceInYears, format } from "date-fns"
+import ptBR from "date-fns/locale/pt-BR"
 import Image from "next/image"
 
 type ExperienceItemProps = {
@@ -17,6 +19,27 @@ export const ExperienceItem = ({ experience }: ExperienceItemProps) => {
     role,
     technologies
   } = experience
+
+  const startDate = new Date(experience.startDate)
+
+  const formattedStartDate = format(startDate, 'MMM yyyy', {locale: ptBR})
+  const formattedEndDate = endDate ? format( new Date (endDate), 'MMM yyyy', {locale: ptBR}) : 'o momento'
+
+  const end = endDate ? new Date(endDate) : new Date()
+
+  const months = differenceInMonths(end, startDate)
+  const years = differenceInYears(end, startDate)
+
+  const monthsRemaining = months % 12
+
+  const formattedDuration =
+    years > 0
+      ? `${years} ano${years > 1 ? 's' : ''}${
+          monthsRemaining > 0
+            ? ` e ${monthsRemaining} mes${monthsRemaining > 1 ? 'es' : ''}`
+            : ''
+        }`
+      : `${months} mes${months > 1 ? 'es' : ''}`
 
   return (
     <div className="grid grid-cols-[40px,1fr] gap-4 md:gap-10">
@@ -45,7 +68,7 @@ export const ExperienceItem = ({ experience }: ExperienceItemProps) => {
           </a>
           <h4 className="text-gray-300">{role}</h4>
           <span className="text-gray-500">
-            out 2022 • O momento • (6 meses)
+            {formattedStartDate} • {formattedEndDate} • ({formattedDuration})
           </span>
           <div className="text-gray-400">
             <RichText content={description.raw} />
