ec2818eb5aa81a991b298bbc3f53bcc478676f2f
richardcastrogois <richard.castrogois@gmail.com>
2025-01-31 22:10:16 -0300

    Nav-bar

--- app/components/header/nav-item.tsx
+++ app/components/header/nav-item.tsx
@@ -1,23 +1,33 @@
-import Link from "next/link"
+'use client'
+
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "../../lib/utils";
 
 type NavItemProps = {
-    label: string
-    href: string
-}
+    label: string;
+    href: string;
+};
 
-export const NavItem = ({label, href }: NavItemProps) => {
+export const NavItem = ({ label, href }: NavItemProps) => {
     const pathname = usePathname();
-
     const isActive = pathname === href;
 
     return (
-        <Link href={href} className={cn(
-            "text-gray-400 flex items-center gap-2 font-medim font-mono"
-        )}>
-            <span className="text-emerald-400">#</span>
+        <Link 
+            href={href} 
+            className={cn(
+                "text-gray-400 flex items-center gap-2 font-medium font-mono hover:text-emerald-400 transition-all  transform hover:scale-110",
+                isActive && "text-emerald-400"
+            )}
+        >
+            <span className={cn(
+                "text-emerald-400",
+                isActive && "text-emerald-400"
+            )}>
+                #
+            </span>
             {label}
         </Link>
-    )
-}
+    );
+};
