da08c762ef4db403dbc6b53da735324eb994df28
richardcastrogois <richard.castrogois@gmail.com>
2025-03-15 01:34:37 -0300

    Certificados Correção

--- app/components/carousel/index.tsx
+++ app/components/carousel/index.tsx
@@ -38,7 +38,7 @@ const certifications = [
   {
     title: "Certificação 5",
     content: "Jornada Full Stack - Hashtag Treinamentos",
-    image: "/certifHashtag.png",
+    image: "/certifHashtag.jpg",
   },
   {
     title: "Certificação 6",
